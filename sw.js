0fedf943aec68ccbd461c272f5dfa7b79b45b45e
faacuviera <facuquito78@gmail.com>
2025-12-15 16:21:08 -0300

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "xtreme-cuentas-v1";
+const CACHE_NAME = "xtreme-cuentas-v2";
 const ASSETS = ["./","./index.html","./styles.css","./app.js","./manifest.json","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",(e)=>{
   e.waitUntil((async()=>{
